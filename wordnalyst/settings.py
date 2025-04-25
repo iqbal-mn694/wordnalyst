dae17c58a84a40c59585ceb7175d6f2fdbbcb32a
iqbal-mn694 <garenahiber134@gmail.com>
2025-04-25 16:58:29 +0700

    configure static file setting

--- wordnalyst/settings.py
+++ wordnalyst/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # 'core',
     'django.contrib.staticfiles',
 ]
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'wordnalyst.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'core' / 'static', 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
